dd862c4fdc0a68dcfdb2a3dec32b806a63a5dcec
Jeremy Long <jeremy.long@gmail.com>
2020-04-10 15:36:35 -0400

    initial

---
+++ src/main/java/com/sample/vuln/sample/SampleConfiguration.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sample.vuln.sample;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class SampleConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/view/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+         pageEncoding="US-ASCII"%>
+<%@taglib prefix="e" uri="https://www.owasp.org/index.php/OWASP_Java_Encoder_Project" %>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+        <title>Vulnerable JSP</title>
+        <script type="text/javascript">
+            function logCall(value) {
+                console.log(value);
+            }
+        </script>
+    </head>
+    <body>
+        <p>This is a test JSP that contains one or more vulnerabilities.</p>
+        <p>Don't <a href="#" onclick="logCall('${e:forJavaScript(param.value)}');">click this link</a>.</p>
+    </body>
+</html>
